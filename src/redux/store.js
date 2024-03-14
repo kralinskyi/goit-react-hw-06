d15582731405f82b5af2fdfa18271da782ac2459
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-14 14:53:04 +0200

    working with different reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,7 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 import initialContacts from "../../initialContacts.json";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { nanoid } from "nanoid";
-
-const initialState = {
-  contacts: {
-    items: initialContacts,
-  },
-  filters: {
-    name: "",
-  },
-};
 
 export const addContact = (name, number) => {
   return {
@@ -37,38 +28,50 @@ export const setFilter = (filterValue) => {
   };
 };
 
-const rootReducer = (state = initialState, action) => {
+const contactsReducer = (
+  state = {
+    items: initialContacts,
+  },
+  action
+) => {
   switch (action.type) {
     case "contacts/addContact":
       return {
         ...state,
-        contacts: {
-          items: [...state.contacts.items, action.payload],
-        },
+        items: [...state.items, action.payload],
       };
 
     case "contacts/deleteContact":
       return {
         ...state,
-        contacts: {
-          items: state.contacts.items.filter(
-            (contact) => contact.id !== action.payload
-          ),
-        },
+        items: state.items.filter((contact) => contact.id !== action.payload),
       };
+    default:
+      return state;
+  }
+};
 
+const filtersReducer = (
+  state = {
+    name: "",
+  },
+  action
+) => {
+  switch (action.type) {
     case "filters/setFilter":
       return {
         ...state,
-        filters: {
-          ...state.filters,
-          name: action.payload,
-        },
+        name: action.payload,
       };
 
     default:
       return state;
   }
 };
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
 
 export const store = createStore(rootReducer, composeWithDevTools());
