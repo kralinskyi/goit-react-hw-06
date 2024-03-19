61be79f77260ba74ae6baaa4c56aed03221a4cf0
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-19 11:10:27 +0200

    repairs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contactsSlice";
 import css from "./Contact.module.css";
 
-export default function Contact({ id, name, number }) {
+export default function Contact({ contact: { id, name, number } }) {
   const dispatch = useDispatch();
 
   const handleDeleteContact = (id) => {
